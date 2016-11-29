d05670232c63267abb5e1ad3773a663c56460159
unknown <090392@DOV2090392.acg.ac.nz>
2016-11-30 11:48:44 +1300

    added more projects

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 var projects = null;
 var cats = null;
-$('#cats').on('click', 'li', function(){
-    //console.log($(this).attr('data-cat'));
-    $('#stalactite-loaded').empty();
-    var cat = $(this).attr('data-cat');
-    var index = $(this).attr('data-index');
+
+var refreshProjects = function(cat, index){
     var jsonCat = cats[parseInt(index)];
     projects = jsonCat[cat];
-    console.log(projects);
     //fill the div with projects
-    for(var i = 0; i < projects.length; i++){
-        $('#stalactite-loaded').append('<div class="item  col-xs-4 col-lg-4"><div class="thumbnail"><img class="group list-group-image" src="' + projects[i].image_url + '" alt=""><div class="caption"><h4 class="group inner list-group-item-heading">' + projects[i].name + '</h4> </div> </div></div>');
+    for (var i = 0; i < projects.length; i++) {
+        $('#stalactite-loaded').append('<div class="item col-xs-4 col-lg-4"><div class="thumbnail"><img class="group list-group-image" src="' + projects[i].image_url + '" alt=""><div class="caption"><h4 class="group inner list-group-item-heading">' + projects[i].name + '</h4> </div> </div></div>');
     }
-});
-$.getJSON( "json/projects.json", function( data ) {
+};
+
+//get the data and init UI
+$.getJSON("json/projects.json", function (data) {
     cats = data.categories;
+    //init UI projects
+    refreshProjects('web','0');
 });
+
+
+
+$('#cats').on('click', 'li', function () {
+    $('#stalactite-loaded').empty();
+    refreshProjects($(this).attr('data-cat'),$(this).attr('data-index'));
+});
+
+
