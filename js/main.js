5c49c27dde4383e772eca39e4a2d0b47feea63ea
Richard Lovell <richjavalabs@gmail.com>
2017-02-22 11:05:39 +1300

    Added underscore and changed JSON

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 var projects = null;
-var cats = null;
+var categories = null;
 $('#cats').on('click', 'li', function(){
     //console.log($(this).attr('data-cat'));
     $('#stalactite-loaded').empty();
-    var cat = $(this).attr('data-cat');
-    var index = $(this).attr('data-index');
-    var jsonCat = cats[parseInt(index)];
-    projects = jsonCat[cat];
-    console.log(projects);
-    //fill the div with projects
-    for(var i = 0; i < projects.length; i++){
-        $('#stalactite-loaded').append('<div class="item  col-xs-4 col-lg-4"><div class="thumbnail"><img class="group list-group-image" src="' + projects[i].image_url + '" alt=""><div class="caption"><h4 class="group inner list-group-item-heading">' + projects[i].name + '</h4> </div> </div></div>');
-    }
+    var category = $(this).attr('data-cat');
+    loadItems(category);
 });
+
+
 $.getJSON( "json/projects.json", function( data ) {
-    cats = data.categories;
+    categories = data.categories;
+    loadItems('Web');
 });
+
+var loadItems = function(categoryName){
+    //var jsonCat = cats[parseInt(index)];
+    var category =  _.findWhere(categories, {'name': categoryName});
+    //projects = jsonCat[cat];
+    console.log(category);
+    //fill the div with projects
+    for(var i = 0; i < category.projects.length; i++){
+        $('#stalactite-loaded').append('<div class="item  col-xs-4 col-lg-4"><div class="thumbnail"><img class="group list-group-image" src="' + category.projects[i].image_url + '" alt=""><div class="caption"><h4 class="group inner list-group-item-heading">' + category.projects[i].name + '</h4> </div> </div></div>');
+    }
+};
